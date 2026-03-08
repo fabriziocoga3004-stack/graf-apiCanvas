cf33ad4f2dedbfbb1ce4494bec1610debb13feff
fabriziocoga3004-stack <fabriziocoga3004@gmail.com>
2026-03-08 17:08:52 -0600

    Triangulo

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ if(canvas.getContext){
 
 const ctx=canvas.getContext("2d");
 
-ctx.fillStyle="#27ae60";
-ctx.fillRect(220,140,220,120);
+ctx.beginPath();
+
+ctx.moveTo(300,100);
+ctx.lineTo(420,300);
+ctx.lineTo(180,300);
+
+ctx.closePath();
+
+ctx.fillStyle="#3498db";
+ctx.fill();
 
 ctx.lineWidth=4;
-ctx.strokeStyle="#145a32";
-ctx.strokeRect(220,140,220,120);
+ctx.strokeStyle="#1f618d";
+ctx.stroke();
 
 }
 
