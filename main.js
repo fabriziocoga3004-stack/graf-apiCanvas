ac4e2996aababcc01f5ca9394a7d6cce0677cbee
fabriziocoga3004-stack <fabriziocoga3004@gmail.com>
2026-03-08 16:50:30 -0600

    Rectangulo

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 function draw(){
 
-const canvas = document.getElementById("canvas");
+const canvas=document.getElementById("canvas");
 
 if(canvas.getContext){
 
-const ctx = canvas.getContext("2d");
+const ctx=canvas.getContext("2d");
+
+ctx.fillStyle="#27ae60";
+ctx.fillRect(220,140,220,120);
+
+ctx.lineWidth=4;
+ctx.strokeStyle="#145a32";
+ctx.strokeRect(220,140,220,120);
 
 }
 
